2744b0ce57ff849e2c027d82f77246e4328de87d
Jair1810 <jair18101@hotmail.com>
2023-12-21 14:55:52 -0600

    Terminado el componente de Imagen del dia.

---
+++ src/components/TodaysImage/TodaysImage.tsx
@@ -0,0 +1,52 @@
+import React, {FC} from 'react';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
+import {PostImage} from '../../types';
+
+const TodaysImage: FC<PostImage> = ({date, title, url}) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image source={{uri: url}} style={styles.image} />
+        {/* Asi se pone una imagen cuando no es local */}
+      </View>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.date}>{date}</Text>
+      <View style={styles.buttoncontainer}>
+        <Button title="View" />
+      </View>
+      {/* Ocupa un view, porque directamente no se pueden aplicar estilos. */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2c449d',
+    marginVertical: 16,
+    marginHorizontal: 24,
+    borderRadius: 32,
+    padding: 16,
+  },
+  image: {
+    width: '100%',
+    height: 190,
+    borderWidth: 2,
+    borderColor: '#ffffff',
+    borderRadius: 32,
+  },
+  title: {
+    color: '#ffffff',
+    fontSize: 20,
+    marginVertical: 12,
+    fontWeight: 'bold',
+  },
+  date: {
+    color: '#ffffff',
+    fontSize: 16,
+  },
+  buttoncontainer: {
+    alignItems: 'flex-end',
+  },
+});
+
+export default TodaysImage;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import Header from '../../components/Header';
+import fetchApi from '../../utils/fetch';
+import TodaysImage from '../../components/TodaysImage';
+import {PostImage} from '../../types';
 
 const Home = () => {
+  //Se tipa el useStage al PostImage
+  const [todaysImage, setTodaysImage] = useState<PostImage>({});
+
+  useEffect(() => {
+    const loadTodaysImage = async () => {
+      try {
+        const todayImageResponse = await fetchApi();
+        //Se guarda en el useState en el cual se va hacer uso despues.
+        setTodaysImage(todayImageResponse);
+      } catch (error) {
+        console.log(error);
+        setTodaysImage({});
+      }
+    };
+
+    loadTodaysImage().catch(null);
+    //Manejo del error evitar que se truene.
+  }, []);
+
+  todaysImage !== undefined && console.log(todaysImage);
+
   return (
     <View style={styles.container}>
       <Header />
+      <TodaysImage {...todaysImage} />
     </View>
   );
 };
