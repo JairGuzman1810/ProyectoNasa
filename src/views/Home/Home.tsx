8bbe6f52e73fe4cbc1c8df1379e34ba76c4b36b3
Jair1810 <jair18101@hotmail.com>
2023-12-21 19:35:48 -0600

    Empezado la consulta a la API de conseguir las ultimas 5 imagenes del dia. Agregado la depedencia date-fns

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 import fetchApi from '../../utils/fetch';
 import TodaysImage from '../../components/TodaysImage';
 import {PostImage} from '../../types';
+import {format, sub} from 'date-fns';
 
 const Home = () => {
   //Se tipa el useStage al PostImage
@@ -21,11 +22,28 @@ const Home = () => {
       }
     };
 
+    const loadLast5DaysImage = async () => {
+      try {
+        //Se consigue la fecha de hoy.
+        const date = new Date();
+        //Se hace formato a la fecha.
+        const todaysDate = format(date, 'yyyy-MM-dd');
+        //Se subtrae 5 dias a la fecha de hoy mediante el metodo sub de la libreria.
+        const fiveDaysAgoDate = format(sub(date, {days: 5}), 'yyyy-MM-dd');
+        //Se pasa los parametros a la URL para conseguir las ultimas imagenes.
+        const lastFiveDaysImagesResponse = await fetchApi(
+          `&start_date=${fiveDaysAgoDate}&end_date=${todaysDate}`,
+        );
+
+        console.log(lastFiveDaysImagesResponse);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadLast5DaysImage().catch(null);
     loadTodaysImage().catch(null);
     //Manejo del error evitar que se truene.
   }, []);
-
-  todaysImage !== undefined && console.log(todaysImage);
 
   return (
     <View style={styles.container}>
